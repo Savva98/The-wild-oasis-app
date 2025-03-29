a38bea5d8ebf3677cf1700866c66be0ee6ce7748
Savva8030 <93856194+Savva98@users.noreply.github.com>
2025-03-29 17:24:31 +0100

    Adding styles and index route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import Dashboard from "../pages/Dashboard";
 import Bookings from "../pages/Bookings";
 import Cabins from "../pages/Cabins";
@@ -11,19 +11,22 @@ import GlobalStyles from "../styles/GlodalStyles";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="bookings" element={<Bookings />} />
-        <Route path="cabins" element={<Cabins />} />
-        <Route path="settings" element={<Settings />} />
-        <Route path="account" element={<Account />} />
-        <Route path="login" element={<Login />} />
-        {/*user.role === 'admin' ? {<Route path="users" element={<Users />}: <Route path='forbidden' element={<Forbiden/>}  */}
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+    <>
       <GlobalStyles />
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Account />} />
+          <Route path="login" element={<Login />} />
+          {/*user.role === 'admin' ? {<Route path="users" element={<Users />}: <Route path='forbidden' element={<Forbiden/>}  */}
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
